67745fdd36ce0164cadb453fb034354e36a08a19
xiuwenli <xiuwenli@github.com>
2018-10-03 03:19:50 +0000

    Initial Script

--- gene2variant2.py
+++ gene2variant2.py
@@ -5,19 +5,19 @@ import json
 import requests
 import urllib
 
-gene_name = raw_input("Please type your gene name, all caps, and press enter: ")
+Lookup = {}
 
-Lookup_geneID={}
+genename = raw_input("Please type your gene name, all caps, and press enter: ")
 
 for each_line_of_text in fileinput.input(['/home/xiuwenli/Assignment5/Homo_sapiens.GRCh37.75.gtf']):
     gene_id_matches = re.findall('gene_id \"(.*?)\";',each_line_of_text)
     gene_name_matches = re.findall('gene_name \"(.*?)\";',each_line_of_text)
-    if gene_id_matches:
-       if gene_name_matches:
-          Lookup_geneID[gene_id_matches[0]] = gene_name_matches[0]
+    if gene_name_matches:
+       if gene_id_matches:
+          Lookup[gene_name_matches[0]] = gene_id_matches[0]
 
 from re import sub
-url = "http://rest.ensembl.org/overlap/id/ENSG00000130203.json?feature=variation"
+url = "http://rest.ensembl.org/overlap/id/" + Lookup[genename] + ".json?feature=variation"
 response = urllib.urlopen(url)
 data = json.loads(response.read())
 for i in range(0, len(data)):
